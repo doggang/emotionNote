3edb3e372e512a38b4dbbad254937c2e2d36332d
doggang <rlaehgusqp@naver.com>
2025-04-01 23:00:27 +0900

    first update

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,40 @@
-import { useState, useRef, useReducer} from 'react'
+import { Routes, Route, Link, useNavigate} from 'react-router-dom';
 import './App.css'
-import Header from './component/Header'
-import Editor from './component/Editor'
-import List from './component/List'
-import Exam from './component/exam'
-import { createContext, useMemo } from 'react'
-
-const reducer=(state, action)=>{
-  switch(action.type){
-    case "CREATE" : return [action.data, ...state];
-    case "UPDATE" : return state.map((item)=>(item.id === action.targetId)?{...item, isDone: !item.isDone,}:item);
-    case "DELETE" : return state.filter((item)=>action.targetId!==item.id);
-  }
-}
-
-export const TodoStateContext = createContext();
-export const TodoDispatchContext = createContext();
-
+import Diary from './pages/Diary';
+import Home from './pages/Home';
+import New from './pages/New';
+import Notfound from './pages/Notfound';
+import {getEmotionImage} from "./util/get-emotion-image.js";
 function App() {
 
-  const mokData = [
-    {
-      id:3,
-      isDone:false,
-      content: "3번입니다다",
-      date: new Date().toDateString()
-    },
-    {
-      id:2,
-      isDone:false,
-      content: "2번입니다다",
-      date: new Date().toDateString()
-    },
-    {
-      id:1,
-      isDone:false,
-      content: "1번입니다다",
-      date: new Date().toDateString()
-    },
-  ]
+  const nav = useNavigate();
 
-  const [todos, dispatch] = useReducer(reducer,mokData);
-
-  const idRef = useRef(4);
-
-  const onCreate = (content)=>{
-    dispatch({
-      type:"CREATE",
-      data : {
-        id:idRef.current++,
-        isDone:false,
-        content:content,
-        date: new Date().toDateString()
-      }
-    })
-    console.log(todos);
+  const onClickBtn = ()=>{
+    nav('/new');
   }
-
-  const onUpdate = (targetId)=>{
-    dispatch({
-      type:"UPDATE",
-      targetId : targetId,
-      }
-    )
-  }
-
-  const onDelete = (targetId)=>{
-    dispatch({
-      type:"DELETE",
-      targetId: targetId,
-    })
-  }
-
-  const memoizedDispatch = useMemo(()=>{
-    return {onCreate, onUpdate, onDelete};
-  },[]);
 
   return (
-    <div className='app'>
-      {/* <Exam /> */}
-      <Header />
-      <TodoStateContext value={todos}>
-        <TodoDispatchContext value={memoizedDispatch}>
-          <Editor />
-          <List />
-        </TodoDispatchContext>
-      </TodoStateContext>
-    </div>
+    <>
+      <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
+      <div>
+        <Link to={'/'}>home</Link>
+        <Link to={'/new'}>new</Link>
+        <Link to={'/diary'}>diary</Link>
+      </div>
+      <button onClick={onClickBtn}>버튼입니다</button>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/new' element={<New />} />
+        <Route path='/diary/:id' element={<Diary />} />
+        <Route path='*' element={<Notfound />} />
+      </Routes>
+    </>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ReactStudy/", // 레포지토리명과 일치해야 함
+  base: "/emotionNote", // 레포지토리명과 일치해야 함
   plugins: [react()],
 })
